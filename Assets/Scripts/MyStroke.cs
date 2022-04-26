d4eba33986ad4690a8f6188aef85ad080fdf7cf2
ataga101 <wentup_3one@outlook.jp>
2022-04-27 01:46:51 +0900

    edited line drawer

---
+++ Assets/Scripts/MyStroke.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stroke : MonoBehaviour
+{
+    List<Vector3> positions;
+    List<Vector3> velocities;
+    List<float> times;
+    int numSamples;
+
+    PolyBezier pb;
+
+    LineRenderer linerenderer;
+
+    public Stroke()
+    {
+        linerenderer = gameObject.AddComponent<LineRenderer>();
+        numSamples = 0;
+    }
+
+    public void addSample(Vector3 position, Vector3 velocity, float time)
+    {
+        //Add sample
+        positions.Add(position);
+        velocities.Add(velocity);
+        times.Add(time);
+        numSamples++;
+
+        //Set up LineRenderer
+        linerenderer = gameObject.GetComponent<LineRenderer>();
+
+        linerenderer.widthMultiplier = 0.1f;
+        linerenderer.startColor = Color.white;
+        linerenderer.endColor = Color.white;
+
+        linerenderer.positionCount = numSamples;
+        linerenderer.SetPosition(numSamples - 1, position);
+    }
+
+    public void endSampling()
+    {
+        Destroy(linerenderer);
+    }
+
+    public void convertToPolyBezier()
+    {
+
+    }
+}
