67b28784fb3e6e89a42cf344f0bc0c6a9ea010a0
ataga101 <wentup_3one@outlook.jp>
2022-04-28 16:32:19 +0900

    [wip] added polybezier conversion & ink sphere

---
+++ Assets/Scripts/InkSphere.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InkSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pose = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
+        transform.position = pose.GetLocalPosition(SteamVR_Input_Sources.RightHand);
+    }
+}
